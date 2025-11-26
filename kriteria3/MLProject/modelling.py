43f9fafafb16ded8808eed5c29e698cbe62b0926
alishaanggranidi <149086589+alishaanggranidi@users.noreply.github.com>
2025-11-26 14:25:50 +0700

    fix

--- kriteria3/MLProject/modelling.py
+++ kriteria3/MLProject/modelling.py
@@ -16,19 +16,18 @@ y_train = train_df["target"]
 X_test = test_df.drop(columns=["target"])
 y_test = test_df["target"]
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
-
-mlflow.set_experiment("Insurance_Cost_Prediction")
+# mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+# mlflow.set_experiment("Insurance_Cost_Prediction")
 
 with mlflow.start_run(run_name="manual_run"):
     mlflow.sklearn.autolog()
 
     model = GradientBoostingRegressor()
     model.fit(X_train,y_train)
+    
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
 
-    # y_pred = model.predict(X_test)
-    # mse = mean_squared_error(y_test, y_pred)
-    # r2 = r2_score(y_test, y_pred)
-
-    # print(f"Mean Squared Error: {mse}")
-    # print(f"R^2 Score: {r2}")
+    print(f"Mean Squared Error: {mse}")
+    print(f"R^2 Score: {r2}")
